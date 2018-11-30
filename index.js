a8002e72a790f8290514ae1629380b68739abe80
ayumitamai97 <ayumitamai97@gmail.com>
2018-11-30 16:04:57 +0900

    Pass json to view

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var app = new Vue({ 
     el: '#app',
     data: {
-        message: 'Hello Vue!'
+      message: 'Hello Vue!'
     }
 });
 var app2 = new Vue({
@@ -16,3 +16,19 @@ var app3 = new Vue({
     seen: true
   }
 })
+
+var chatMsgInJson = {
+  "text": "Welcome to oss chat!!!",
+  "userId": "1",
+  "chatId": 1,
+  "image": "http://spring.io/img/homepage/icon-spring-boot.svg",
+  "createdAt": "2018-11-28T11:46:33",
+  "id": 1
+}
+
+var app4 = new Vue({ 
+  el: '#jsonSample',
+  data: {
+    chat_message_in_json: chatMsgInJson
+  }
+});
